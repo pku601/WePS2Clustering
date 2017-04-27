f7026de9de7a7c7c595f160300593fee4bd171b7
lihongqiang <415200973@qq.com>
2017-04-27 16:32:34 +0800

    merge

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-__author__ = 'lhq'
+import cPickle as pickle
+import os
+from tfidf_based_feature import get_similarity
+# tfidf_dir = "./training/tfidf"
+#
+# name = "Abby_Watkins"
+#
+# file = os.path.join(tfidf_dir, name+".pkl")
+#
+# words_dict = pickle.load(open(file, "r"))
+#
+# rank_vec = words_dict["rank_vec"]
+# words_tfidf = words_dict["words_tfidf"]
+#
+# print len(words_tfidf), len(words_tfidf[0])
+
+a = [1, 1, 0]
+b = [1, 1, 0]
+print get_similarity(a,b)
